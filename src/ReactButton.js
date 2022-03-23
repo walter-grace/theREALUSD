3ca6c0f514493e1e116b3407a2d83ee7c55da382
Walter Grace <31444892+walter-grace@users.noreply.github.com>
2022-03-23 16:27:15 -0700

    Update ReactButton.js

--- src/ReactButton.js
+++ src/ReactButton.js
@@ -9,7 +9,7 @@ export default function ReactButton() {
             <AwesomeButton
             type="secondary"
             size="large"
-            href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/112829198486691756376904202910570877934169287029014742543201552265834770989061"
+            href="https://opensea.io/assets/matic/0x2953399124f0cbb46d2cbacd8a89cf0599974963/63367272381273427994526977373537561235187732922829644273149988688510907842561"
             >
                🍔 Go
             </AwesomeButton>
